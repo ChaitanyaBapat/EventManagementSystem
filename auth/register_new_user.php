473e1188a8f8d367341265a9effcd2b4b2744219
hello@example.com <hello@example.com>
2017-10-25 21:45:04 +0530

    attempt to upload phpexcel

--- auth/register_new_user.php
+++ auth/register_new_user.php
@@ -1,3 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>EMS HomePage</title>
+
+	<!-- Meta Information -->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- W.CSS Stylesheet -->
+	<link rel="stylesheet" type="text/css" href="../css/w3.css">
+	<link rel="stylesheet" href="../css/w3-colors-highway.css">
+
+	<!-- Script For jQuery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+		<!-- Internal CSS -->
+		<style type="text/css">
+			@media screen and (max-resolution:420dpi) and (max-device-width:720px) {
+				.myclass {
+					display: flex;
+				}
+			}
+		</style>
+</head>
+<body>
+	<div class="w3-row w3-highway-green" style="padding-top: 10px; padding-bottom: 10px; position: sticky;  top:0px;z-index:1000">
+
+		<div class="w3-col l8 w3-container w3-highway-green" style="text-align: center;">
+			<a href="./index.php" style="text-decoration: none; font-size: 30px;">Event Management System</a>
+		</div>
+
+		<div class="w3-col l4 w3-row w3-highway-green w3-mobile myclass">
+
+			<?php
+				session_start();
+
+				//If Admin is logged in, redirect to admin's homepage
+				// Here, the redirect address is set to events/index.php
+				//The script in that page redirects it to admin homepage (admin_homepage.php)
+
+				if ( isset ( $_SESSION['admin_login'] ) && $_SESSION['admin_login'] ) {
+
+					header ( "location:../events/index.php" );
+					exit ();
+
+				}
+
+				//If User is logged in, echo Logout Button
+				//Else, echo Login and register buttons
+
+				if( isset ( $_SESSION['login'] ) && $_SESSION['login'] ) {
+
+					header ( "location:../index.php" );
+					exit ();
+
+				}
+			?>
+			<!-- Contact Us Button -->
+			<a class="w3-mobile w3-btn w3-hover-white w3-round-large w3-border-highway-green" style="float:left;font-size: 16px;" href="#/">Contact Us</a>
+			<a class="w3-mobile w3-btn w3-hover-white w3-round-large w3-border-highway-green" style="float:left;font-size: 16px;" href="../register/register.php">Register</a>
+		</div>
+	</div>				
+
+	<!-- Main Body Starts Here -->
+
+	<!-- Container For Body -->
+	<div class="w3-mobile">
+		<div class="w3-container">
 <?php
 	if (!isset($_SESSION)) {
 		session_start();
@@ -90,4 +160,5 @@
 			}
 		}
 	}
-?>
+?></div></div>
+</body>
